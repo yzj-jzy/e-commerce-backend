e7d7bac5ed9056747fcaf4b54ba358c1b7fd4b3a
zhijianyang <zyan3784@uni.sydney.edu.au>
2024-02-01 21:01:36 +1100

    new

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ require('dotenv').config({path:'./.env'});
 const createCheckoutSession = require('./API/checkout');
 
 const app = express();
-const port = 8080;
 
 app.use(express.json());
 app.use(cors({origin:true}));
 
 app.get('/',(req,res)=>{res.send('Hello World')});
 app.post('/createCheckoutSession',createCheckoutSession);
-
-app.listen(port,()  => console.log('Server listening on port',port));
